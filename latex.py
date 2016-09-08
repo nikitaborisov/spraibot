789b969f27b37b95e9db31cb861a6a8fcfb23789
Nikita Borisov <git@nikita.ca>
2016-09-08 11:38:10 -0500

    unescape messages

--- latex.py
+++ latex.py
@@ -1,4 +1,5 @@
 from urllib.parse import quote
+from html import unescape
 from slackbot.bot import listen_to, respond_to
 import json
 
@@ -7,6 +8,6 @@ import json
 def render_latex(message, latex):
     # based on https://github.com/nicolewhite/SlackTeX/blob/master/slacktex/views.py
     latex_url = "http://chart.apis.google.com/chart?cht=tx&chl={latex}".format(
-        latex=quote(latex))
+        latex=quote(unescape(latex)))
     message.send_webapi('', json.dumps([{"image_url": latex_url,
         "fallback": "Latex rendering fail?"}]))
