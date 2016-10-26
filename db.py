f97c89ec44aa1f03109a2e63aefc51a10d1a0e2c
Nikita Borisov <git@nikita.ca>
2016-10-26 10:25:55 -0500

    PEP8

--- db.py
+++ db.py
@@ -1,20 +1,21 @@
 from sqlalchemy import create_engine, Date, String, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 import os
 
 engine = create_engine(os.environ.get('DATABASE_URL'), echo=True)
 
-from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class Deadline(Base):
     __tablename__ = 'deadlines'
 
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     date = Column(Date)
     item = Column(String)
 
-from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker(bind=engine)
